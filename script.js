824194001bac276c9294ae4723afc975e21720bb
Ricky <rickyjoet@gmail.com>
2021-05-01 21:43:59 -0700

    time date on top and change color of timetable

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ console.log(moment());
 let m = moment().format('LLLL');
 console.log(m);
 
+// var currentDay = document.getElementById ("currentDay");
+
+// currentDay.textContent = "Hello";
+
+
+console.log(moment().format('h a'));
+
+var timeBlocks = document.querySelectorAll(".time");
+var businessHours = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
+
+for (var i = 0; i < timeBlocks.length; i++) {
+    // console.log(timeBlocks[i]);
+    timeBlocks[i].textContent = businessHours[i];
+}
